6ecd35cbc3f42f01eb0a74202526edd1e1e313e2
Omar Ali <excalibur1987@gmail.com>
2022-02-16 14:07:56 +0300

    feat: :tada: First commit with working storybook and svelete config

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import sveltePreprocess from "svelte-preprocess";
+import path from "path";
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+  vite: {
+    resolve: {
+      alias: {
+        $lib: path.resolve("./src/lib"),
+        $components: path.resolve("./src/lib/components"),
+      },
+    },
+  },
+};
